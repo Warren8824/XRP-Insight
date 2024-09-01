3ace9fb88bc90c3311ab375fb8215f9a027aedcc
Warren8824 <warrenbebbington88@gmail.com>
2024-09-01 19:06:13 +0100

    updated __main__.py to ensure unittest searches directories recursively.

--- tests/__main__.py
+++ tests/__main__.py
@@ -2,7 +2,10 @@ import unittest
 
 if __name__ == '__main__':
     test_loader = unittest.TestLoader()
-    test_suite = test_loader.discover('tests', pattern='test_*.py')
+    test_suite = test_loader.discover('.', pattern='test_*.py', top_level_dir='.')
 
-    runner = unittest.TextTestRunner()
-    runner.run(test_suite)
+    runner = unittest.TextTestRunner(verbosity=2)
+    result = runner.run(test_suite)
+
+    # Exit with non-zero code if tests failed
+    exit(not result.wasSuccessful())
