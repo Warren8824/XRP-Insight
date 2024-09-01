c21c3908d72eda27336aed5e9d33e7b853ee15d5
Warren8824 <warrenbebbington88@gmail.com>
2024-09-01 14:36:07 +0100

    added logger.py util script to setup log file

---
+++ src/utils/logger.py
@@ -0,0 +1,23 @@
+import logging
+import os
+from logging.handlers import RotatingFileHandler
+
+
+def setup_logger(name, log_file, level=logging.INFO):
+    formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+
+    handler = RotatingFileHandler(log_file, maxBytes=10*1024*1024, backupCount=5)
+    handler.setFormatter(formatter)
+
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+    logger.addHandler(handler)
+
+    return logger
+
+# Usage
+log_directory = 'logs'
+if not os.path.exists(log_directory):
+    os.makedirs(log_directory)
+
+logger = setup_logger('xrp_market_bot', 'logs/xrp_market_bot.log')
