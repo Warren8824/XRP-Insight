e71ab75b7f2e7b295d59f7c7eba6ee3eed700d1e
Warren8824 <warrenbebbington88@gmail.com>
2024-09-04 18:18:28 +0100

    updated and reformatted ohlcv_15_min_data.py.

--- src/models/ohlcv_15_min_data.py
+++ src/models/ohlcv_15_min_data.py
@@ -1,18 +1,22 @@
-from sqlalchemy import Column, Integer, Float, DateTime
-from sqlalchemy.sql import func
+from sqlalchemy import Column, Integer, Float, DateTime, CheckConstraint
 from src.models.base import Base
+
 
 class OHLCV15Data(Base):
     __tablename__ = "ohlcv_15_data"
 
     id = Column(Integer, primary_key=True, index=True)
-    timestamp = Column(DateTime(timezone=True), index=True)
-    open = Column(Float)
-    high = Column(Float)
-    low = Column(Float)
-    close = Column(Float)
-    volume = Column(Float)
-    price_change = Column(Float)
+    timestamp = Column(DateTime(timezone=True), index=True, unique=True)
+    open = Column(Float, nullable=False)
+    high = Column(Float, nullable=False)
+    low = Column(Float, nullable=False)
+    close = Column(Float, nullable=False)
+    volume = Column(Float, nullable=False)
+    price_change = Column(Float, nullable=False)
+
+    __table_args__ = (
+        CheckConstraint('high >= low', name='check_high_low'),
+    )
 
     def __repr__(self):
-        return f"<OHLCV15Data(id={self.id}, timestamp={self.timestamp}, price_usd={self.price_usd})>"
+        return f'<OHLCV15Data(id={self.id}, timestamp={self.timestamp}, close={self.close})>'
