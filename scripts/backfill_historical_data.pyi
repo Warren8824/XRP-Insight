4e0923d7abe0fa9e2815010439da56287b912e6a
Warren8824 <warrenbebbington88@gmail.com>
2024-09-11 12:16:45 +0100

    created basic bf_data function to collect 90 days of ohlcv data to initiate system.

---
+++ scripts/backfill_historical_data.pyi
@@ -0,0 +1,23 @@
+from datetime import datetime, timedelta, timezone
+from src.data_collection.collector import collect_historical_data
+from src.models.base import SessionLocal
+from src.utils.logger import utils_logger as logger
+
+def bf_data(start_date, end_date):
+    db = SessionLocal()
+    try:
+        collect_historical_data(db, start_date, end_date)
+    finally:
+        db.close()
+
+
+if __name__ == "__main__":
+    # Get the current date and time
+    end_date = datetime.now(timezone.utc)
+
+    # Calculate the date 90 days before the current date
+    start_date = end_date - timedelta(days=90)
+
+    logger.info(f"Starting historical data backfill from {start_date} to {end_date}")
+    bf_data(start_date, end_date)
+    logger.info("Historical data backfill completed")
