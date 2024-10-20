3af947d3958b3da500e8a6a311cb096d5fcf7697
Warren8824 <warrenbebbington88@gmail.com>
2024-10-20 19:07:16 +0100

    format input spacing.

--- scripts/init_db.py
+++ scripts/init_db.py
@@ -128,7 +128,7 @@ def prompt_user_for_action(engine):
             else:
                 scripts_logger.info(f"Table {table_name} has no data.")
     update_schema = (
-        input("Do you require all tables to be reset for schema updates? y/n")
+        input("Do you require all tables to be reset for schema updates? y/n: ")
         .strip()
         .lower()
     )
